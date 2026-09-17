22eb0859a762c4e190db6d2b21e99523af0176d9
Nikita Kasian <c00313128@setu.ie>
2026-09-17 23:23:30 +0000

    Complete Lab 1

---
+++ lab01/student.py
@@ -0,0 +1,7 @@
+name = input("Name: ")
+student_number = input("Student Number: ")
+year = input("Year: ")
+
+print("Name: " + name)
+print("Student Number: " + student_number)
+print("Year: " + year)
